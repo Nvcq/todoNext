5220d98d2eab8e4c903c1d6b5d09fa902576e9b4
Nicolas Mopin <Nicolas.mopin@edu.devinci.fr>
2021-12-13 14:33:50 +0100

    :poop:

---
+++ app/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Navbar.module.css';
+
+export default function NavBar() {
+    return(
+        <div className={styles.navcontainer}>
+            <p>dandan</p>
+            <p>floflo</p>
+            <p>mini popow</p>
+        </div>
+    )
+
+}
